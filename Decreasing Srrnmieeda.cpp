0bf902358cc470ba44f6e25c6d61985897b68e36
Avi Kumrawat <56340999+avikt18@users.noreply.github.com>
2020-10-19 17:04:35 +0530

    Decreasing Srrnmieeda

---
+++ Decreasing Srrnmieeda.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+   {
+        long long a,b;
+        cin>>a>>b;
+    if(2*a-1>=b)
+    {
+        cout<<b<<"\n";
+    }
+    else
+    {
+        cout<<-1<<"\n";
+    }
+   }
+    return 0;
+
+
+
+
+
+}
