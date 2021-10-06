538b1e194f07ecd32e9d050a403e33d2822f5fbb
Suman kumar <59159432+sumj25@users.noreply.github.com>
2021-10-06 19:56:05 +0530

    create find remainder file

---
+++ findremainder.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main(){
+  
+  int t; 
+  cin>>t; 
+  
+  while(t--)
+  {
+    int a, b; 
+    cin>>a>>b; 
+    
+    int remainder = a%b; 
+    cout<<remainder<<endl; 
+  }
+  
+  return 0;
+}
